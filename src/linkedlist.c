dc415006dd7f1e1fe2a03472e9595312b81fcd0a
leonardo <leo.pohlmann@hotmail.com>
2023-12-11 14:20:19 -0300

    linkedlist append

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Element {
     int index;
@@ -24,7 +25,27 @@ void LLRead(struct LinkedList *ll){
             LLPrintElement(current);
         }
     }
-    
+}
+
+
+void LLAppend(struct LinkedList *ll, int value){
+    struct Element *e = malloc(sizeof(*e));
+    if (ll->head == NULL) {
+        e->index = 0;
+        e->data = value;
+        e->next = NULL;
+        ll->head = e;
+    } else {
+        struct Element *current = ll->head;
+        while (current->next) {
+            current = current->next;
+        }
+        e->index = current->index + 1;
+        e->data = value;
+        e->next = NULL;
+        current->next = e;
+    }
+
 }
 
 int main(){
@@ -32,8 +53,7 @@ int main(){
     struct Element e2 = {2, 5, &e1};
     struct Element e3 = {1, 1, &e2};
     struct LinkedList ll = {&e3};
-
+    LLAppend(&ll, 23);
     LLRead(&ll);
-
     return 0;
 }
