e9341f866cf91fbd985f729d360940ca9ce8413b
leonardo <leo.pohlmann@hotmail.com>
2024-01-04 17:53:35 -0300

    teste append

--- test.c
+++ test.c
@@ -1,8 +1,10 @@
 #include "linkedlist.h"
 #include <criterion/criterion.h>
+#include <criterion/internal/assert.h>
+#include <criterion/internal/test.h>
 #include <stdio.h>
 
-Test(ll_length, ll_length_tamanho_1) {
+Test(ll, ll_length_tamanho_1) {
   struct LinkedList a;
   struct Element b;
   b.data = 1;
@@ -11,13 +13,13 @@ Test(ll_length, ll_length_tamanho_1) {
   cr_assert(LL_Length(&a) == 1);
 }
 
-Test(ll_length, ll_length_tamanho_0) {
+Test(ll, ll_length_tamanho_0) {
   struct LinkedList a;
   a.head = NULL;
   cr_assert(LL_Length(&a) == 0);
 }
 
-Test(ll_length, ll_length_tamanho_maior_1) {
+Test(ll, ll_length_tamanho_maior_1) {
   struct Element c;
   c.data = 5;
   c.next = NULL;
@@ -29,4 +31,25 @@ Test(ll_length, ll_length_tamanho_maior_1) {
   struct LinkedList a;
   a.head = &b;
   cr_assert(LL_Length(&a) == 2);
+}
+
+Test(ll, ll_append_lista_vazia) {
+    struct LinkedList list;
+    list.head = NULL;
+
+    LL_Append(&list, 2);
+
+    cr_assert(list.head->data == 2);
+}
+
+Test(ll, ll_append_lista_preenchida) {
+    struct Element e1;
+    e1.data = 10;
+    e1.next = NULL;
+    struct LinkedList list;
+    list.head = &e1;
+    
+    LL_Append(&list, 10);
+
+    cr_assert(list.head->next->data == 10);
 }
